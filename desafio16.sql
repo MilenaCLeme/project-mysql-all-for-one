2d49d7fa68744ebca87d2b68ee212ebd75c02eab
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-19 18:03:15 -0300

    Requisitos 15 ao 19

---
+++ desafio16.sql
@@ -0,0 +1,1 @@
+SELECT submitted_date FROM northwind.purchase_orders where submitted_date between '2006-01-26 00:00:00' and '2006-03-31 23:59:59';
---
+++ desafio19.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(*) FROM northwind.orders WHERE employee_id in (5, 6) AND shipper_id = 2;
